fd476fe1d320febaccd1d764b219550ddd83fd75
Michael Smart <michaelsmart88@gmail.com>
2017-10-03 11:49:09 +0100

    lint types file, still need to figure out the convention with these

--- src/types.ts
+++ src/types.ts
@@ -1,31 +1,31 @@
 export interface Channel {
-    members: any[],
-    name: string,
-    id: StringConstructor
-};
+    members: any[];
+    name: string;
+    id: StringConstructor;
+}
 
 export interface UserResponse {
-    user: Member
-};
+    user: Member;
+}
 
 export interface ImOpenResponse {
-    channel: Channel
-};
+    channel: Channel;
+}
 
 export interface Member {
-    is_bot: boolean,
-    name: string,
-    id: string,
-    real_name: string
-};
+    is_bot: boolean;
+    name: string;
+    id: string;
+    real_name: string;
+}
 
 export interface Suggestion {
-    user_id: string,
-    value: string
+    user_id: string;
+    value: string;
 }
 
 export interface SentimentExtract {
-    text: string,
-    _id?: string,
-    suggestions: Suggestion[]
+    text: string;
+    _id?: string;
+    suggestions: Suggestion[];
 }
