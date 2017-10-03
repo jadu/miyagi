9c9367e78e34b1bc795345680b9a5da36242e560
Michael Smart <michaelsmart88@gmail.com>
2017-10-03 10:26:45 +0100

    remove scratch file

--- src/bot/bot.ts
+++ src/bot/bot.ts
@@ -1,7 +1,10 @@
-import { WebClient, RTMClient } from '@slack/client';
+import { WebClient, RtmClient } from '@slack/client';
 import { Logger, transports, LoggerInstance } from 'winston';
-import { Channel, Member } from '../types';
+import { Channel, Member, SentimentExtract } from '../types';
 import { getHumansFromChannel, getGeneralChannel, sendDirectMessage } from './botUtils';
+import { MongoClient, Db, MongoError } from 'mongodb';
+import { getRandomExtracts } from '../server/serverUtils';
+import { DB_URL } from '../server/server';
 
 const logger: LoggerInstance = new Logger({
     level: 'debug',
@@ -15,21 +18,50 @@ if (!token) {
     process.exit(1);
 }
 
-const web: WebClient = new WebClient(token);
-const rtm: RTMClient = new RTMClient(token);
+// Slack Web API client
+const api: WebClient = new WebClient(token);
+// Using this just to indicate whether the Bot is online
+const rtm: RtmClient = (new RtmClient(token)).start();
 
 // APPLICATION
 (async function () {
     let humans: Member[] = [];
+    let extracts: SentimentExtract[] = [];
+    let database: Db;
 
     // Get list of humans
     try {
-        const general: Channel = await getGeneralChannel(web, logger);
+        const general: Channel = await getGeneralChannel(api, logger);
        
-        humans = await getHumansFromChannel(web, logger, general); 
+        humans = await getHumansFromChannel(api, logger, general); 
         logger.info(`Got ${humans.length} human${humans.length === 1 ? '' : 's'} from #general`);
         logger.debug(humans.map(human => human.real_name).join(', '));
     } catch (error) {
         logger.error(error);
     }
+
+    // Connect to database
+    try {
+        database = await MongoClient.connect(DB_URL);
+    } catch (error) {
+        logger.error(error);
+    }
+
+    // Get extracts
+    // MongoClient.connect(DB_URL, async (error: MongoError, db: Db) => {
+    //     if (error) {
+    //         logger.error(error.message);
+    //     } else {
+    //         logger.info(`Conntected to "${DB_URL}"`);
+    //         extracts = await getRandomExtracts(db, logger, humans.length);
+    //     }
+    // });
+
+    console.log(extracts);
+
+    extracts.forEach((extract: SentimentExtract) => {
+        console.log(extract);
+    });
 })();
+
+
