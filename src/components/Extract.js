b555ffbc498507783384a3cc41556d6e743bd432
Michael Smart <michaelsmart88@gmail.com>
2017-11-02 13:07:02 +0000

    add replace method to turn asteriks to redaction characters

--- src/components/Extract.js
+++ src/components/Extract.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+function replaceCharacters (text, original, replacement) {
+    const re = new RegExp(`\\${original}`, 'gi');
+
+    return text.replace(re, replacement);
+}
+
 export default function Extract ({ text }) {
     const parsedText = text.split(/\n/g)
         .map((line, index) =>
-            <p key={line.slice(0, 5) + index} className="extract__copy">{ line }</p>
+            <p key={line.slice(0, 5) + index} className="extract__copy">{ replaceCharacters(line, '*', '█') }</p>
         );
 
     return (
