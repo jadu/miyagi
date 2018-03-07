c1ac4067b709b170cc459c0d93f6da72d48a741f
Michael Smart <michaelsmart88@gmail.com>
2018-03-07 10:47:41 +0000

    remove db connection from async function, errors are being swallowed

--- src/bin/createFixtures.ts
+++ src/bin/createFixtures.ts
@@ -36,66 +36,70 @@ const { file, database_host, database, collection }: Arguments = Object.assign(
 // Build connection string for Mongo
 const databasePath = `${database_host}/${database}`;
 
-MongoClient.connect(databasePath, async (error, db) => {
+MongoClient.connect(databasePath, (error, db) => {
     if (error) {
-        throw new Error(error.message);
-    }
-
-    // Start timer
-    recordTimer.start();
-
-    // Get / Set collection
-    const col = await db.collection(collection);
-
-    // Drop collection
-    const sure = await question(rl, `Are you sure you want to delete the contents of "${collection}" in the database "${databasePath}"? (y/n): `);
-
-    switch (sure) {
-        case 'y':
-            await col.deleteMany({});
-            process.stdout.write(`Removed documents from "${collection}"`);
-            break;
-        case 'n':
-        default:
-            readline.cursorTo(process.stdout, 0);
-            process.stdout.write('Collection not deleted. Exiting process.\n');
-            process.exit(0);
-    }
-
-    // Setup streaming
-    const JSONStream = makeSource() as any;
-    // Store insertion promises
-    const insertions: Promise<any>[] = [];
-    // Pipe read stream to JSONStreamer
-    fs.createReadStream(file).pipe(JSONStream.input);
-
-    JSONStream.on('error', (error) => {
-        throw new Error(error.message);
-    });
-
-    JSONStream.on('stringValue', async (value: string) => {
-        // Add value to collection
-        insertions.push(col.insertOne({
-            text: value,
-            suggestions: [],
-            has_suggestion: false
-        }));
-    });
-
-    // Stream ended
-    JSONStream.on('end', async () => {
-        // Wait for db insertions to finish
-        await Promise.all(insertions);
-        // Close db connection
-        await db.close();
-        // End timer
-        recordTimer.end();
-        // Print summary
-        process.stdout.write('\n');
-        process.stdout.write(`${insertions.length} extracts successfully inserted into "${databasePath}" in the "${collection}" collection (${recordTimer.print()})\n`);
-        // Exit
+        process.stdout.write(`\nError: ${error.message}`);
+        process.stdout.write(`\nMake sure you have started your MongoDB Service!\n\n`);
         process.exit(0);
-    });
+    }
+
+    (async function () {
+            // Get / Set collection
+        const col = await db.collection(collection);
+
+        // Drop collection
+        const sure = await question(rl, `Are you sure you want to delete the contents of "${collection}" in the database "${databasePath}"? (y/n): `);
+
+        // Start timer
+        recordTimer.start();
+
+        switch (sure) {
+            case 'y':
+                await col.deleteMany({});
+                process.stdout.write(`Removed all documents from "${collection}"`);
+                break;
+            case 'n':
+            default:
+                readline.cursorTo(process.stdout, 0);
+                process.stdout.write('Collection not deleted. Exiting process.\n');
+                process.exit(0);
+        }
+
+        // Setup streaming
+        const JSONStream = makeSource() as any;
+        // Store insertion promises
+        const insertions: Promise<any>[] = [];
+        // Pipe read stream to JSONStreamer
+        fs.createReadStream(file).pipe(JSONStream.input);
+
+        JSONStream.on('error', (error) => {
+            throw new Error(error.message);
+        });
+
+        JSONStream.on('stringValue', async (value: string) => {
+            // Add value to collection
+            insertions.push(col.insertOne({
+                text: value,
+                suggestions: [],
+                has_suggestion: false
+            }));
+        });
+
+        // Stream ended
+        JSONStream.on('end', async () => {
+            // Wait for db insertions to finish
+            await Promise.all(insertions);
+            // Close db connection
+            await db.close();
+            // End timer
+            recordTimer.end();
+            // Print summary
+            process.stdout.write('\n');
+            process.stdout.write(`${insertions.length} extracts successfully inserted into "${databasePath}" in the "${collection}" collection (${recordTimer.print()})\n`);
+            // Exit
+            process.exit(0);
+        });
+    })()
 });
 
 
